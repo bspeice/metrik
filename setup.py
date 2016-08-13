d2fd30304b034c9c2a3e83e76a8b6bce2fe25569
Bradlee Speice <bradlee.speice@gmail.com>
2016-08-13 00:09:57 -0400

    Make doubly-sure pytz is installed

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ setup(
         'luigi >= 2.2.0',
         'requests >= 2.11.0',
         'six >= 1.10.0',
-        'pymongo >= 3.2'
+        'pymongo >= 3.2',
+        'pytz >= 2016.6.1'
     ],
     setup_requires=[
         'pytest_runner'
