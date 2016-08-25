d3c9e100316a2bb2e68dae6b68f7d7333d3d63f8
Bradlee Speice <bradlee.speice@gmail.com>
2016-08-25 07:41:11 -0400

    Remove cron string test

--- test/test_batch.py
+++ test/test_batch.py
@@ -11,7 +11,3 @@ class BatchTest(TestCase):
         for flow_name, flow_class in flows.items():
             assert flow_class(present=present,
                               live=live).get_schedule() is not None
-
-    def test_cron_string(self):
-        cron_string = build_cron_file()
-        assert cron_string[-1] == '\n'
