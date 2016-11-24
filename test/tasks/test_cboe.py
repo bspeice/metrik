3ba12bf224aa695a7978d9ffadfe267d44d1d9b6
bspeice <bspeice.nc@gmail.com>
2016-11-24 14:26:13 -0500

    Primary holding is now T, not HCP

--- test/tasks/test_cboe.py
+++ test/tasks/test_cboe.py
@@ -8,5 +8,3 @@ class CboeTest(TestCase):
     def test_optionable_list(self):
         companies = CboeOptionableList.retrieve_data()['companies']
         assert len(companies) > 2000
-        # TODO: Get lists of companies from ETF holdings and verify that they
-        # can be found here as well - this should be a superset
